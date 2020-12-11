100a46e47be49c6a5bb704f89c0601fc83067684
blueriver212 <indy.brownhall@gmail.com>
2020-12-11 15:30:51 +0000

    Fix: Github error fix

--- highest_point.py
+++ highest_point.py
@@ -1,10 +1,9 @@
 import pyproj
 from Shapely import Point, Polygon
 
+# British National Grid Projection
+bng = pyproj.Proj(init='epsg:27700')
 
-
-#British National Grid Projection
-bng=pyproj.Proj(init='epsg:27700')
 
 def create_buffer(coord):
     """
@@ -13,9 +12,20 @@ def create_buffer(coord):
     :return:
     """
 
+    # First take the coordinate and turn into a point with shapely
     coord_input = Point[coord[0], coord[1]]
 
+    # Transform the point to a British National Grid
 
-    #this is a comment to show how i am editing the file
+    # Create a 5km buffer around the point
+
+    # this is a comment to show how i am editing the file
+
+def highest_point(coord, buffer):
+    pass
+
+    # Find the highest point in that buffer zone.
+    # Return the coordinate of the highest point in the 5km buffer.
+    # There is a tool called clip/extract by mask, that will cut the elevation raster into a smaller one.
 
 
