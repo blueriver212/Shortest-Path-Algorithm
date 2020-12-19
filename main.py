99c1ec1be619fd777d04fabce5d20b084613b905
blueriver212 <indy.brownhall@gmail.com>
2020-12-19 17:02:06 +0000

    Fix: Adding comments for when the program is running slowly so user understands what is happening

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import os
 
 
 def main():
+
+    ## TASK 1
     # creating window using the UserInput class
-    root = UserInput()
-    coord = root.user_input()
-    root.window.mainloop()
+    # root = UserInput()
+    # coord = root.user_input()
+    # root.window.mainloop()
 
 
+
+    # TASK 2
     pt = Point(450000, 92000)
     root = os.path.dirname(os.getcwd())
     island_file = 'isle_of_wight.shp'
@@ -21,22 +25,25 @@ def main():
     elevation_path = os.path.join(root, 'Material', 'elevation', elevation_file)
 
     out_loc = r'clip.tif'
+    print('Clipping your input raster to a 5km Buffer...')
     hp = HighestPoint(pt, island_path, elevation_path, out_loc)
     clipped_path = hp.clip_elevation()
     highest_point_in_area = hp.find_highest_point(clipped_path)
+    print(f'The highest point in your area is {highest_point_in_area}')
 
+    # TASK 3
     # example of input coordinate, this will change when we merge the tasks together
-    coord = Point(434000, 90000)
-    highest = Point(439619, 85800)
 
     # Get the error handling from sep document
     json_file = Errors.json_input()
     itn = ITN(json_file)
+    print('Calculating the nearest road nodes to you...')
 
     # find the closest node for closest algorithm
-    node_near_user = itn.nearest_node(coord)
-    node_near_high_point = itn.nearest_node(highest)
-    print(node_near_user, node_near_high_point)
+    node_near_user = itn.nearest_node(pt)
+    node_near_high_point = itn.nearest_node(highest_point_in_area)
+    print(f'The node nearest you is {node_near_user} \n'
+          f'The node nearest to the highest point is {node_near_high_point}')
 
 
 if __name__ == "__main__":
