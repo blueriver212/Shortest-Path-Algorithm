06c3b11d87cf977151587adf57166fddab2cc53f
blueriver212 <indy.brownhall@gmail.com>
2020-12-22 13:03:46 +0000

    Fix: Adding changes to main for ideas for Task 4

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from shapely.geometry import Point, Polygon
 from highest_point import *
 from GUI import *
 import os
-
+from test import *
 
 def main():
 
@@ -14,15 +14,15 @@ def main():
     # coord = root.user_input()
     # root.window.mainloop()
 
-
-
     # TASK 2
     pt = Point(450000, 92000)
     root = os.path.dirname(os.getcwd())
     island_file = 'isle_of_wight.shp'
     elevation_file = 'SZ.asc'
+    itn = 'solent_itn.json'
     island_path = os.path.join(root, 'Material', 'shape', island_file)
     elevation_path = os.path.join(root, 'Material', 'elevation', elevation_file)
+    itn_path = os.path.join(root, 'Material', 'itn', itn)
 
     out_loc = r'clip.tif'
     print('Clipping your input raster to a 5km Buffer...')
@@ -45,6 +45,11 @@ def main():
     print(f'The node nearest you is {node_near_user} \n'
           f'The node nearest to the highest point is {node_near_high_point}')
 
+    nr = NearestRoad(elevation_path, node_near_user, node_near_high_point)
+    get_road = nr.get_road(itn_path)
+    print(f'The road of your journey is: {get_road}')
+    dijkstra = nr.get_nearest_path()
+    print(dijkstra)
 
 if __name__ == "__main__":
     main()
